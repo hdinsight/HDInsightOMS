6d956b22cc970be9bb8815b10c52c92df13afeb9
Ryan Waters <rywater@microsoft.com>
2017-05-12 16:46:18 -0700

    Added simple python script to dynamically create worker JMX configurations based on Ambari configuration

---
+++ monitoring/storm/workergen.py
@@ -0,0 +1,69 @@
+
+""" Generates the CollectD JMX configuration for Storm worker nodes
+    based off of values pulled from a local Ambari instance"""
+import urllib2
+import os
+import json
+from string import Template
+from optparse import OptionParser
+
+CONFIG_API = "/api/v1/cluster/configuration"
+JSON_HEADER = {"Content-Type": "application/json"}
+
+CONNECTION_TEMPLATE = Template("""
+<Connection>
+  Host "$host_name"
+  ServiceURL "service:jmx:rmi:///jndi/rmi://$service_host/jmxrmi"
+  InstancePrefix "$instance"
+  Collect "memory-heap"
+</Connection>
+""")
+
+
+def get_topology_configuration(config_host):
+    request = urllib2.Request(config_host + CONFIG_API)
+    for key, val in JSON_HEADER.items():
+        request.add_header(key, val)
+
+    response = urllib2.urlopen(request)
+    return json.load(response)
+
+
+def get_supervisor_slots(configuration):
+    return configuration['supervisor.slots.ports']
+
+
+def create_connection_block(slot):
+    return CONNECTION_TEMPLATE.substitute(host_name='localhost-{}'.format(slot),
+                                          service_host='localhost:{}'.format(
+                                              slot),
+                                          instance='worker-{}'.format(slot))
+
+
+def get_worker_jmx_template():
+    template_text = open(os.path.join("templates", "worker_jmx.conf"))
+    return Template(template_text.read())
+
+
+def generate_worker_config():
+    parser = OptionParser()
+    parser.add_option("-u", "--url", dest="host", default='http://localhost:8744',
+                      help="Ambari server host. e.g. http://localhost:8744")
+
+    (options, _) = parser.parse_args()
+    config_host = options.host
+
+    configuration = get_topology_configuration(config_host)
+    slots = get_supervisor_slots(configuration)
+    worker_connections = ""
+    for slot in slots:
+        worker_connections += create_connection_block(slot)
+
+    print 'Created worker blocks for slots {}'.format(slots)
+
+    jmx_template = get_worker_jmx_template()
+    open(os.path.join('worker', 'collectd_oms_worker_jmx.conf'),
+         'w').write(jmx_template.substitute(worker_connections=worker_connections))
+
+
+generate_worker_config()
